3ac62ac11581671058745dd9a3b197fe0a8cb5f4
FRK-SANDKAKE\linda <frk.sandkake@gmail.com>
2022-12-06 05:19:59 +0100

    functioning and responsive navBar / burgerMenu. JS for burgerNav in src/js/utils folder

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
 import '../style.css';
+import {menuOpen, menuClose} from "./utils/mobileNavControll";
+
+const burgerBtn = document.getElementById('hamburger');
+burgerBtn.addEventListener('click', menuOpen);
+
+const closeNavBtn = document.getElementById('closeNavBtn');
+closeNavBtn.addEventListener('click', menuClose)

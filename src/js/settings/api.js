a463cebf76e8026d50c9f9ae7def7295a2de7227
FRK-SANDKAKE\linda <frk.sandkake@gmail.com>
2022-12-13 17:41:29 +0100

    SignUpUser API fetch success, async in fetchSignup, validation functions in validation

--- src/js/settings/api.js
+++ src/js/settings/api.js
@@ -1,7 +1,17 @@
 const AUCTION_API_BASE_URL = 'https://api.noroff.dev/api/v1/auction';
 
-// LISTING
+// LISTINGS
 const GET_LISTINGS_URL = `${AUCTION_API_BASE_URL}/listings`;
 const GET_12_LISTINGS_BIDS_TAGS_URL = `${AUCTION_API_BASE_URL}/listings?_bids=true&tags=string&limit=12`;
+// const GET_LISTINGS_DESC_URL = `${AUCTION_API_BASE_URL}/listings?sort=created&sortOrder=desc`;
 
-export { AUCTION_API_BASE_URL, GET_LISTINGS_URL, GET_12_LISTINGS_BIDS_TAGS_URL };
+// AUTHENTICATION
+const POST_SIGNUP_URL = `${AUCTION_API_BASE_URL}/auth/register`;
+// const POST_LOGIN_URL = `${AUCTION_API_BASE_URL}/auth/login`;
+
+// PROFILE
+// const GET_PROFILE_URL = `${AUCTION_API_BASE_URL}/auth/profiles`;
+// const POST_BIDS_URL = `${AUCTION_API_BASE_URL}/${id}bids`;
+
+
+export { AUCTION_API_BASE_URL, POST_SIGNUP_URL, GET_LISTINGS_URL, GET_12_LISTINGS_BIDS_TAGS_URL };

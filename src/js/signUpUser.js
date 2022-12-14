63c36b84f58eaf5e9ab1bf05f4af2cf43cca842f
FRK-SANDKAKE\linda <frk.sandkake@gmail.com>
2022-12-14 16:07:05 +0100

    Login form (modal) ready for API fetch. Signup modal added. Fixed after lint run: changed let to const, export default on timeDateFormat, moved checkInputsSignUp() above signup addeventlist.

--- src/js/signUpUser.js
+++ src/js/signUpUser.js
@@ -1,6 +1,6 @@
-import {POST_SIGNUP_URL} from "./settings/api";
+import { POST_SIGNUP_URL } from './settings/api';
 import { checkLength, emailValid, matchPasswords } from './utils/validation';
-import postSignUp from "./utils/fetchSignup";
+import postSignUp from './utils/fetchSignup';
 
 const signupForm = document.getElementById('signUpForm');
 const errorMessage = document.getElementById('errorMessageSignUp');
@@ -13,77 +13,77 @@ const errorPasswordCreate = document.getElementById('errorPasswordCreate');
 const passwordVerify = document.getElementById('passwordVerify');
 const errorPasswordVerify = document.getElementById('errorPasswordVerify');
 
-
-signupForm.addEventListener('submit', function(e) {
-  e.preventDefault();
-
-  if (checkInputsSignUp()) {
-    const userData = {
-      name: userName.value,
-      email: email.value,
-      password: password.value,
-    };
-
-    const USER_SIGNUP_URL = `${POST_SIGNUP_URL}`;
-    postSignUp(userData, USER_SIGNUP_URL)
-      .then((response) => {
-      signupForm.reset();
-      location.href = '/';
-    }).catch((errMessage) => {
-      errorMessage.innerHTML = `${errMessage}`;
-    });
-  } else {
-    errorMessage.innerHTML = 'Validation failed..';
-  }
-});
-
 function checkInputsSignUp() {
-  let validInput = false;
-  if (checkLength(userName.value, 2)) {
-    userName.classList.add('text-green-500', 'border-green-500');
-    errorName.classList.add('hidden');
-    validInput = true;
-  } else {
-    userName.classList.add('text-red-500', 'border-red-500');
-    errorName.classList.remove('hidden');
-  }
+    let validInput = false;
+    if (checkLength(userName.value, 2)) {
+        userName.classList.add('text-green-500', 'border-green-500');
+        errorName.classList.add('hidden');
+        validInput = true;
+    } else {
+        userName.classList.add('text-red-500', 'border-red-500');
+        errorName.classList.remove('hidden');
+    }
 
-  if (checkLength(email.value, 1)) {
-    errorEmail.classList.add('hidden');
-    validInput = true;
-  } else {
-    email.classList.add('text-red-500', 'border-red-500');
-    errorEmail.classList.remove('hidden');
-  }
+    if (checkLength(email.value, 1)) {
+        errorEmail.classList.add('hidden');
+        validInput = true;
+    } else {
+        email.classList.add('text-red-500', 'border-red-500');
+        errorEmail.classList.remove('hidden');
+    }
 
-  if (checkLength(email.value, 1) && emailValid(email.value) === true) {
-    email.classList.add('text-green-500', 'border-green-500');
-    errorEmail.classList.add('hidden');
-    validInput = true;
-  } else if (checkLength(email.value, 1) && emailValid(email.value) !== true) {
-    email.classList.add('text-red-500', 'border-red-500');
-    errorEmail.classList.remove('hidden');
-  }
+    if (checkLength(email.value, 1) && emailValid(email.value) === true) {
+        email.classList.add('text-green-500', 'border-green-500');
+        errorEmail.classList.add('hidden');
+        validInput = true;
+    } else if (checkLength(email.value, 1) && emailValid(email.value) !== true) {
+        email.classList.add('text-red-500', 'border-red-500');
+        errorEmail.classList.remove('hidden');
+    }
 
-  if (checkLength(password.value, 8)) {
-    password.classList.add('text-green-500', 'border-green-500');
-    errorPasswordCreate.classList.add('hidden');
-    validInput = true;
-  } else {
-    password.classList.add('text-red-500', 'border-red-500');
-    errorPasswordCreate.classList.remove('hidden');
-  }
+    if (checkLength(password.value, 8)) {
+        password.classList.add('text-green-500', 'border-green-500');
+        errorPasswordCreate.classList.add('hidden');
+        validInput = true;
+    } else {
+        password.classList.add('text-red-500', 'border-red-500');
+        errorPasswordCreate.classList.remove('hidden');
+    }
 
-  if (matchPasswords(password.value, passwordVerify.value)) {
-    password.classList.add('text-green-500', 'border-green-500');
-    passwordVerify.classList.add('text-green-500', 'border-green-500');
-    errorPasswordVerify.classList.add('hidden');
-    validInput = true;
-  } else {
-    password.classList.add('text-red-500', 'border-red-500');
-    passwordVerify.classList.add('text-red-500', 'border-red-500');
-    errorPasswordVerify.classList.remove('hidden');
-  }
+    if (matchPasswords(password.value, passwordVerify.value)) {
+        password.classList.add('text-green-500', 'border-green-500');
+        passwordVerify.classList.add('text-green-500', 'border-green-500');
+        errorPasswordVerify.classList.add('hidden');
+        validInput = true;
+    } else {
+        password.classList.add('text-red-500', 'border-red-500');
+        passwordVerify.classList.add('text-red-500', 'border-red-500');
+        errorPasswordVerify.classList.remove('hidden');
+    }
 
-  return validInput;
+    return validInput;
 }
+
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (checkInputsSignUp()) {
+        const userData = {
+            name: userName.value,
+            email: email.value,
+            password: password.value,
+        };
+
+        const USER_SIGNUP_URL = `${POST_SIGNUP_URL}`;
+        postSignUp(userData, USER_SIGNUP_URL)
+            .then((response) => {
+                signupForm.reset();
+                location.href = '/';
+            })
+            .catch((errMessage) => {
+                errorMessage.innerHTML = `${errMessage}`;
+            });
+    } else {
+        errorMessage.innerHTML = 'Validation failed..';
+    }
+});
--- src/js/utils/fetchSignup.js
+++ src/js/utils/fetchSignup.js
@@ -1,18 +1,17 @@
 async function postSignUp(userData, POST_SIGNUP_URL) {
-  const response = await fetch(POST_SIGNUP_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(userData),
-  });
+    const response = await fetch(POST_SIGNUP_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+    });
 
-  const jsonResponse = await response.json();
-  if (response.ok) {
-  return jsonResponse;
-  } else {
+    const jsonResponse = await response.json();
+    if (response.ok) {
+        return jsonResponse;
+    }
     throw Error(`I'm sorry but ${jsonResponse.errors[0].message}`);
-  }
 }
 
 export default postSignUp;

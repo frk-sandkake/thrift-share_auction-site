262834b66b16fc3c881d437d2f3e2d0d24ac2396
Linda Sandaker <frk.sandkake@gmail.com>
2023-11-08 22:03:39 +0100

    test upload thrift

--- src/js/createThrift.js
+++ src/js/createThrift.js
@@ -1,6 +1,6 @@
 import { POST_THRIFT_URL } from './settings/api';
 import { getToken } from './utils/storage';
-import { checkLength, checkImgUrl } from './utils/validation';
+import { checkLength } from './utils/validation';
 
 const addThriftForm = document.getElementById('addThriftForm');
 const thriftTitle = document.getElementById('thriftTitle');
@@ -10,7 +10,6 @@ const errorThriftDescription = document.getElementById('errorThriftDescription')
 const thriftImage1 = document.getElementById('thriftImageUrl1');
 const thriftImage2 = document.getElementById('thriftImageUrl2');
 const thriftImage3 = document.getElementById('thriftImageUrl3');
-const errorThriftImage = document.getElementById('errorThriftImageUrl');
 const thriftTag1 = document.getElementById('thriftTag1');
 const thriftTag2 = document.getElementById('thriftTag2');
 const thriftTag3 = document.getElementById('thriftTag3');
@@ -23,38 +22,29 @@ addThriftForm.addEventListener('submit', (e) => {
 
     let validInputAddThrift = false;
     if (checkLength(thriftTitle.value, 4)) {
-        thriftTitle.classList.add('text-green-500', 'border-green-500');
+        thriftTitle.classList.add('text-green-400', 'border-green-400');
         errorThriftTitle.classList.add('hidden');
         validInputAddThrift = true;
     } else {
-        thriftTitle.classList.add('text-red-500', 'border-red-500');
+        thriftTitle.classList.add('text-red-400', 'border-red-400');
         errorThriftTitle.classList.remove('hidden');
     }
 
     if (checkLength(thriftDescription.value, 7)) {
-        thriftDescription.classList.add('text-green-500', 'border-green-500');
+        thriftDescription.classList.add('text-green-400', 'border-green-400');
         errorThriftDescription.classList.add('hidden');
         validInputAddThrift = true;
     } else {
-        thriftDescription.classList.add('text-red-500', 'border-red-500');
+        thriftDescription.classList.add('text-red-400', 'border-red-400');
         errorThriftDescription.classList.remove('hidden');
     }
 
-    if (checkImgUrl(thriftImage1.value || thriftImage1.value === '')) {
-        thriftImage1.classList.add('text-green-500', 'border-green-500');
-        errorThriftImage.classList.add('hidden');
-        validInputAddThrift = true;
-    } else {
-        thriftImage1.classList.add('text-red-500', 'border-red-500');
-        errorThriftImage.classList.remove('hidden');
-    }
-
     if (endTime.value) {
-        endTime.classList.add('text-green-500', 'border-green-500');
+        endTime.classList.add('text-green-400', 'border-green-400');
         errorEndTime.classList.add('hidden');
         validInputAddThrift = true;
     } else {
-        endTime.classList.add('text-red-500', 'border-red-500');
+        endTime.classList.add('text-red-400', 'border-red-400');
         errorEndTime.classList.remove('hidden');
     }
 
@@ -86,7 +76,7 @@ addThriftForm.addEventListener('submit', (e) => {
                 errorMessageAddThrift.innerHTML = 'Your thrift is listed!';
             } else {
                 const errMessage = await response.json();
-                errorMessageAddThrift.innerHTML = `${errMessage}`;
+                errorMessageAddThrift.innerHTML = `${errMessage[0]}`;
             }
         }
         postThrift().then();
